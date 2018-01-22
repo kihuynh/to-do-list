302ba6791d97185501b03a5e9879b1f614aea32c
kaila goff and kimberly huynh <kimberly@>
2018-01-22 15:52:04 -0800

    add remove function

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,31 @@ function List (task) {
 }
 
 
-$(document).ready(function() {
+ $(document).ready(function() {
   $("form.list").submit(function(event) {
-    event.preventDefault();
+     event.preventDefault();
 
     var inputTask = $("input#task").val();
-
     var newTask = new List(inputTask);
 
-  //  $('<input type="checkbox"/>').append('li');
-    $("ul#list").append("<li><input class='form-check-input' type='checkbox'><span class='task'>" + newTask.task + "</span></li>");
+   $("ul#list").append("<li><input class='form-check-input' type='checkbox' id='checkbox'><span class='task'>" + newTask.task + "</span></li>");
 
-    $("ul#list").click(function() {
-      $('li').hide();
-    });
+
+   $("ul#list").children("li").click(function() {
+     $(this).remove();
+   });
+
 
     $(".result").show();
   });
 });
+
+
+//  $("#checkbox").change(function() {
+//    if (this.checked) {
+//      value= $(this).val();
+//      $("#list li").filter(function() {
+//        return $(this).text() == $this.value;
+//      }) .remove();
+//    }
+//  });
